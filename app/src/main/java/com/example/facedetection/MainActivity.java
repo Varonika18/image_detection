814e7d490d6ef6084673ed68300d6bfa4077b8b4
Varonika <105442598+Varonika18@users.noreply.github.com>
2022-09-17 19:58:08 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/facedetection/MainActivity.java
+++ app/src/main/java/com/example/facedetection/MainActivity.java
@@ -48,12 +48,12 @@ public class MainActivity extends AppCompatActivity {
             @SuppressLint("NewApi")
             @Override
             public void onClick(View view) {
-                // Launch camera if we have permission
+                
                 if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
                     Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                     startActivityForResult(cameraIntent, 1);
                 } else {
-                    //Request camera permission if we don't have it.
+                   
                     requestPermissions(new String[]{Manifest.permission.CAMERA}, 100);
                 }
             }
@@ -64,16 +64,15 @@ public class MainActivity extends AppCompatActivity {
         try {
             ModelUnquant model =  ModelUnquant.newInstance(getApplicationContext());
 
-            // Creates inputs for reference.
             TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 224, 224, 3}, DataType.FLOAT32);
             ByteBuffer byteBuffer = ByteBuffer.allocateDirect(4 * imageSize * imageSize * 3);
             byteBuffer.order(ByteOrder.nativeOrder());
 
-            // get 1D array of 224 * 224 pixels in image
+          
             int [] intValues = new int[imageSize * imageSize];
             image.getPixels(intValues, 0, image.getWidth(), 0, 0, image.getWidth(), image.getHeight());
 
-            // iterate over pixels and extract R, G, and B values. Add to bytebuffer.
+           
             int pixel = 0;
             for(int i = 0; i < imageSize; i++){
                 for(int j = 0; j < imageSize; j++){
@@ -86,12 +85,12 @@ public class MainActivity extends AppCompatActivity {
 
             inputFeature0.loadBuffer(byteBuffer);
 
-            // Runs model inference and gets result.
+            
             ModelUnquant.Outputs outputs = model.process(inputFeature0);
             TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
 
             float[] confidences = outputFeature0.getFloatArray();
-            // find the index of the class with the biggest confidence.
+    
             int maxPos = 0;
             float maxConfidence = 0;
             for(int i = 0; i < confidences.length; i++){
@@ -110,10 +109,10 @@ public class MainActivity extends AppCompatActivity {
             confidence.setText(s);
 
 
-            // Releases model resources if no longer used.
+            
             model.close();
         } catch (IOException e) {
-            // TODO Handle the exception
+           
         }
     }
 
